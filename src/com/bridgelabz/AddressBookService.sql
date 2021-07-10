03768fb61ec09941c6e8c9eb6975a015d4d71c61
Roshni1998 <roshnimali847@gmail.com>
2021-07-10 17:12:26 +0530

    Create various attributes to Address Book Service DB

--- src/com/bridgelabz/AddressBookService.sql
+++ src/com/bridgelabz/AddressBookService.sql
@@ -22,3 +22,32 @@ mysql> show databases;
 
 mysql> USE address_book_service;
 Database changed
+
+/* UC-2 */
+mysql> CREATE table address_book (
+    -> first_name VARCHAR(100) NOT NULL,
+    -> last_name VARCHAR(100) NOT NULL,
+    -> address VARCHAR(200) NOT NULL,
+    -> city VARCHAR(50) NOT NULL,
+    -> state VARCHAR(50) NOT NULL,
+    -> zip INT NOT NULL,
+    -> phone_number INT(10) NOT NULL,
+    -> email_id VARCHAR(20) NOT NULL,
+    -> primary key (phone_number)
+    -> );
+Query OK, 0 rows affected, 1 warning (1.79 sec)
+
+mysql> desc address_book;
++--------------+--------------+------+-----+---------+-------+
+| Field        | Type         | Null | Key | Default | Extra |
++--------------+--------------+------+-----+---------+-------+
+| first_name   | varchar(100) | NO   |     | NULL    |       |
+| last_name    | varchar(100) | NO   |     | NULL    |       |
+| address      | varchar(200) | NO   |     | NULL    |       |
+| city         | varchar(50)  | NO   |     | NULL    |       |
+| state        | varchar(50)  | NO   |     | NULL    |       |
+| zip          | int          | NO   |     | NULL    |       |
+| phone_number | int          | NO   | PRI | NULL    |       |
+| email_id     | varchar(20)  | NO   |     | NULL    |       |
++--------------+--------------+------+-----+---------+-------+
+8 rows in set (0.15 sec)

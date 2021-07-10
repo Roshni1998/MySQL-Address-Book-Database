524217576a897228df5a84d038968a5128447856
Roshni1998 <roshnimali847@gmail.com>
2021-07-10 16:04:29 +0530

    Create a Address Book Service DB

---
+++ src/com/bridgelabz/AddressBookService.sql
@@ -0,0 +1,24 @@
+/* Welcome to MySQL Address_Book_Service Database */
+
+/* UC-1 */
+mysql> Create database Address_Book_Service;
+Query OK, 1 row affected (0.30 sec)
+
+mysql> show databases;
++--------------------------+
+| Database                 |
++--------------------------+
+| address_book_service     |
+| classicmodels            |
+| employee_payroll_service |
+| information_schema       |
+| mysql                    |
+| payroll_service          |
+| performance_schema       |
+| student_personal_details |
+| sys                      |
++--------------------------+
+9 rows in set (0.14 sec)
+
+mysql> USE address_book_service;
+Database changed
